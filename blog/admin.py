12655f0627910e933fcd01963581f0e320365089
yu-send <julia.send@gmail.com>
2019-11-30 16:56:03 +0200

    Added publish and delite buttons.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_date', 'author')
+    list_filter = ['published_date']
+    search_fields = ['text', 'title']
+
+
+
+
+
+
+
+
+
+admin.site.register(Post, PostAdmin)
